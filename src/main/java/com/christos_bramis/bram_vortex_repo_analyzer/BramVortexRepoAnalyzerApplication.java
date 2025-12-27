41321b19f65f2cf3afb3c8361c3f1358de598234
DaBram <bramis04chris@gmail.com>
2025-12-27 19:37:34 +0200

    init

---
+++ src/main/java/com/christos_bramis/bram_vortex_repo_analyzer/BramVortexRepoAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.christos_bramis.bram_vortex_repo_analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BramVortexRepoAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BramVortexRepoAnalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/christos_bramis/bram_vortex_repo_analyzer/BramVortexRepoAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.christos_bramis.bram_vortex_repo_analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BramVortexRepoAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
